166d953dc9eecd91d721c0b3c2e58ef99a4ad9a3
Dreo92 <erraixo@gmail.com>
2014-06-29 02:27:15 +0300

    Update ps3_hangman.py

--- Hangman/ps3_hangman.py
+++ Hangman/ps3_hangman.py
@@ -1,14 +1,3 @@
-# 6.00 Problem Set 3
-# 
-# Hangman game
-#
-
-# -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
-
 import random
 import string
 
@@ -107,35 +96,4 @@ def hangman(secretWord):
     else:
         print "Sorry, you ran out of guesses. The word was else."
     
-    '''
-    secretWord: string, the secret word to guess.
-
-    Starts up an interactive game of Hangman.
-
-    * At the start of the game, let the user know how many 
-      letters the secretWord contains.
-
-    * Ask the user to supply one guess (i.e. letter) per round.
-
-    * The user should receive feedback immediately after each guess 
-      about whether their guess appears in the computers word.
-
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
-      user has not yet guessed.
-
-    Follows the other limitations detailed in the problem write-up.
-    '''
-    # FILL IN YOUR CODE HERE...
-
-
-
-
-
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
-
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
+   
